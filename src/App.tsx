31a0794bb8c1276386c6c865c8ebdf79d09c7829
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-02-06 18:38:11 +0900

    8.14 You need to watch this

--- src/App.tsx
+++ src/App.tsx
@@ -6,84 +6,44 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
-  justify-content: center;
   align-items: center;
-  font-size: 28px;
+  justify-content: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box: Variants = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
-// 니꼬쌤꺼에서 버그 있어서 댓글에 있던 아래 주소 참고함
-// https://github.com/jijiseong/react_animation/blob/8e624ce9f5680b2cfc03bb92c014fc9e2fe929a4/src/components/Slider.tsx
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState({ value: false });
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
 
-  const nextPlease = () => {
-    setBack({ value: false });
-  };
-  const prevPlease = () => {
-    setBack({ value: true });
-  };
-  useEffect(() => {
-    if (back.value) {
-      setVisible((cur) => (cur !== 1 ? cur - 1 : cur));
-    } else {
-      setVisible((cur) => (cur !== 10 ? cur + 1 : cur));
-    }
-  }, [back]);
   return (
-    <Wrapper>
-      <AnimatePresence custom={back.value} mode="wait">
-        <Box
-          custom={back.value}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
